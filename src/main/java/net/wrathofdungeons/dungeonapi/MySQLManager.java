347ea7d3123e5c4f4d3ee634ab422e9aa7d03a4d
Zeryther <zeryther@gmail.com>
2017-09-10 21:33:57 +0200

    Added MySQL Manager

---
+++ src/main/java/net/wrathofdungeons/dungeonapi/MySQLManager.java
@@ -0,0 +1,82 @@
+package net.wrathofdungeons.dungeonapi;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.sql.*;
+
+public class MySQLManager {
+    private static MySQLManager instance;
+    private static Connection con;
+
+    private static String host;
+    private static String user;
+    private static String password;
+    private static String database;
+    private static int port;
+
+    public static MySQLManager getInstance(){
+        return instance;
+    }
+
+    static {
+        if(instance == null) instance = new MySQLManager();
+    }
+
+    public MySQLManager(){
+        load();
+    }
+
+    public Connection getConnection(){
+        checkConnection();
+
+        return this.con;
+    }
+
+    public void load(){
+        FileConfiguration config = DungeonAPI.getInstance().getConfig();
+
+        this.host = config.getString("mysql.host");
+        this.user = config.getString("mysql.user");
+        this.password = config.getString("mysql.password");
+        this.database = config.getString("mysql.database");
+        this.port = config.getInt("mysql.port");
+    }
+
+    private void openConnection(){
+        try {
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+            con = DriverManager.getConnection(url,user,password);
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void checkConnection(){
+        try {
+            if(this.con == null || !this.con.isValid(2)){
+                openConnection();
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void closeResources(ResultSet rs, PreparedStatement ps){
+        try {
+            if(rs != null) rs.close();
+            if(ps != null) ps.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void unload(){
+        try {
+            if(con != null && this.con.isValid(2)){
+                con.close();
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
