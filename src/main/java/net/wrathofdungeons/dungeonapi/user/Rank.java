3181d78ff9577f7359f3e309bb3a5c3b409db283
Zeryther <zeryther@gmail.com>
2017-09-11 12:01:39 +0200

    Added ranks

--- src/main/java/net/wrathofdungeons/dungeonapi/user/Rank.java
+++ src/main/java/net/wrathofdungeons/dungeonapi/user/Rank.java
@@ -1,5 +1,45 @@
 package net.wrathofdungeons.dungeonapi.user;
 
+import org.bukkit.ChatColor;
+
 public enum Rank {
-    USER,DONATOR,ADMIN
+    ADMIN(5,"Admin",ChatColor.DARK_RED),
+    GM(4,"Game Master",ChatColor.YELLOW),
+    MODERATOR(3,"Moderator",ChatColor.GREEN),
+    DONATOR(2,"Donator",ChatColor.DARK_GREEN),
+    USER(1,"User",ChatColor.WHITE);
+
+    private int id;
+    private String name;
+    private ChatColor color;
+
+    Rank(int id, String name, ChatColor color){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ChatColor getColor(){
+        return this.color;
+    }
+
+    public static Rank fromTag(String t){
+        for(Rank r : values()) if(r.toString().equalsIgnoreCase(t)) return r;
+
+        return Rank.USER;
+    }
+
+    public static Rank fromID(int i){
+        for(Rank r : values()) if(r.getID() == i) return r;
+
+        return Rank.USER;
+    }
 }
