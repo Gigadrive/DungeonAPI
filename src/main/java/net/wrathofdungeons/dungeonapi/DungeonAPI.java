bf9908323058fd74bdf6da3002a06983e4aa6fb9
Zeryther <zeryther@gmail.com>
2017-09-14 23:30:45 +0200

    Added isInRectangle methods

--- src/main/java/net/wrathofdungeons/dungeonapi/DungeonAPI.java
+++ src/main/java/net/wrathofdungeons/dungeonapi/DungeonAPI.java
@@ -23,6 +23,7 @@ import org.inventivetalent.menubuilder.inventory.InventoryListener;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DungeonAPI extends JavaPlugin {
@@ -263,6 +264,37 @@ public class DungeonAPI extends JavaPlugin {
         loc2.setY(loc2.getWorld().getMaxHeight());
 
         return blocksFromTwoPoints(loc1,loc2);
+    }
+
+    public static boolean isInRectangle(Player player, Location loc1, Location loc2){
+        double[] dim = new double[2];
+
+        dim[0] = loc1.getX();
+        dim[1] = loc2.getX();
+        Arrays.sort(dim);
+        if(player.getLocation().getX() > dim[1] || player.getLocation().getX() < dim[0])
+            return false;
+
+        dim[0] = loc1.getY();
+        dim[1] = loc2.getY();
+        Arrays.sort(dim);
+        if(player.getLocation().getY() > dim[1] || player.getLocation().getY() < dim[0])
+            return false;
+
+        dim[0] = loc1.getZ();
+        dim[1] = loc2.getZ();
+        Arrays.sort(dim);
+        if(player.getLocation().getZ() > dim[1] || player.getLocation().getZ() < dim[0])
+            return false;
+
+        return true;
+    }
+
+    public static boolean isInRectangleMaximized(Player player, Location loc1, Location loc2){
+        loc1.setY(0);
+        loc2.setY(loc2.getWorld().getMaxHeight());
+
+        return isInRectangle(player,loc1,loc2);
     }
 
     public static DungeonAPI getInstance(){
