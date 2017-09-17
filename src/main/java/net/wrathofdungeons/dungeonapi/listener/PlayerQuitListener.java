0085e24f8e753b045b4b6990a04a3b5092998ec0
Zeryther <zeryther@gmail.com>
2017-09-17 15:59:50 +0200

    Delay storage removal

--- src/main/java/net/wrathofdungeons/dungeonapi/listener/PlayerQuitListener.java
+++ src/main/java/net/wrathofdungeons/dungeonapi/listener/PlayerQuitListener.java
@@ -1,19 +1,26 @@
 package net.wrathofdungeons.dungeonapi.listener;
 
+import net.wrathofdungeons.dungeonapi.DungeonAPI;
 import net.wrathofdungeons.dungeonapi.user.User;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerQuitListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent e){
         Player p = e.getPlayer();
 
-        if(User.isLoaded(p)){
-            User.getUser(p).saveData();
-            User.STORAGE.remove(p);
-        }
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                if(User.isLoaded(p)){
+                    User.getUser(p).saveData();
+                    User.STORAGE.remove(p);
+                }
+            }
+        }.runTaskLaterAsynchronously(DungeonAPI.getInstance(),20);
     }
 }
